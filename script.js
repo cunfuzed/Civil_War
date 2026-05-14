3873cef2fc8924b73f88270ed6330981beb1b9a0
Cunfuzed <throwmessagehi@gmail.com>
2026-05-13 23:20:04 -0700

    Add changeScene function to manage scene visibility

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function changeScene(scene) {
 
+  document.querySelectorAll('.scene').forEach(el => {
+    el.classList.remove('visible');
+  });
+
+  document.getElementById(scene).classList.add('visible');
+}
