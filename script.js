3fc5692c6221436faaa85036a756a173cb9d559a
Cunfuzed <throwmessagehi@gmail.com>
2026-05-15 13:12:35 -0700

    Implement basic quiz functionality with buttons

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+
+const questionText = document.getElementById("questionText");
+
+const buttons = [button1, button2, button3, button4];
+
+let questions = [
+  ["what is 6", "6", "5", "2", "1"]
+];
+
+let currentQuestion;
+
 function changeScene(scene) {
 
   document.querySelectorAll('.scene').forEach(el => {
@@ -5,4 +20,42 @@ function changeScene(scene) {
   });
 
   document.getElementById(scene).classList.add('visible');
+
+  if(scene === "game"){
+    generateQuestion();
+  }
 }
+
+function generateQuestion() {
+
+  currentQuestion =
+    questions[Math.floor(Math.random() * questions.length)];
+
+  questionText.innerHTML = currentQuestion[0];
+
+  let answers = currentQuestion.slice(1);
+
+  answers.sort(() => Math.random() - 0.5);
+
+  for(let i = 0; i < 4; i++){
+    buttons[i].innerHTML = answers[i];
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+
+  buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+      if(button.innerHTML === currentQuestion[1]){
+        console.log("correct");
+      } else {
+        console.log("incorrect");
+      }
+
+    });
+
+  });
+
+});
